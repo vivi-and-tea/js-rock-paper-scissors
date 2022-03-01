600232f9ab09af089889cc2d50b38364cc5644ce
neonritual <vtshore@gmail.com>
2022-03-01 21:06:05 +0900

    Finish basic console/ptompy-only function.

--- script.js
+++ script.js
@@ -1,35 +1,102 @@
-// Rock Paper Scissors!
+// Rock Paper Scissors! But this time I realize I'm supposed to use strings instead of integers.
 
-const playerScore = 0;
-const computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 // Randomizing computer's choice.
 function computerPlay() {
-  return Math.floor(Math.random() * 3 + 1);
+  const possibleChoices = ["rock", "paper", "scissors"];
+  let randomIndex = Math.floor(Math.random() * 3);
+  return possibleChoices[randomIndex];
 }
 
-// Getting values for player and pc choices.
-const computerSelection = computerPlay();
-const playerSelection = 3;
-
-// Function to play a single round.
-function playRound(pc, player) {
-  if (pc === player) {
-    console.log("It's a tie! " + pc + player);
-  } else if (pc === 2 && player === 3) {
-    console.log("Computer wins: " + pc + " " + player);
-  } else if (pc === 3 && player === 1) {
-    console.log("Computer wins: " + pc + " " + player);
-  } else if (pc < player) {
-    console.log("Computer wins: " + pc + " " + player);
+function playerInput() {
+  input = prompt("Please choose one: Rock, Paper, or Scissors.").toLowerCase();
+  if (input === "rock" || input === "scissors" || input == "paper") {
+    return input;
   } else {
-    console.log("Player wins: " + pc + " " + player);
+    alert("Sorry, that's not a valid choice.");
+    playerInput();
+  }
+}
+
+// Function to play a single round
+function playRound(pc, player) {
+  if (pc == player) {
+    console.log("You both chose " + pc + ". It's a tie!");
+    console.log(
+      "Player Score: " +
+        playerScore +
+        ". Computer Score: " +
+        computerScore +
+        "."
+    );
+  } else if (pc == "rock" && player == "scissors") {
+    computerScore += 1;
+    console.log(
+      "You chose " + player + " and the computer chose " + pc + ". You lose!"
+    );
+    console.log(
+      "Player Score: " +
+        playerScore +
+        ". Computer Score: " +
+        computerScore +
+        "."
+    );
+  } else if (pc == "scissors" && player == "paper") {
+    computerScore += 1;
+    console.log(
+      "You chose " + player + " and the computer chose " + pc + ". You lose!"
+    );
+    console.log(
+      "Player Score: " +
+        playerScore +
+        ". Computer Score: " +
+        computerScore +
+        "."
+    );
+  } else if (pc == "paper" && player == "rock") {
+    computerScore += 1;
+    console.log(
+      "You chose " + player + " and the computer chose " + pc + ". You lose!"
+    );
+    console.log(
+      "Player Score: " +
+        playerScore +
+        ". Computer Score: " +
+        computerScore +
+        "."
+    );
+  } else {
+    playerScore += 1;
+    console.log(
+      "You chose " + player + " and the computer chose " + pc + ". You win!"
+    );
+    console.log(
+      "Player Score: " +
+        playerScore +
+        ". Computer Score: " +
+        computerScore +
+        "."
+    );
   }
 }
 
 // Function to loop for 5 rounds, tracking points.
 function game() {
-  for (let i = 0; i < 5; i++) {}
+  for (let rounds = 0; rounds < 5; rounds++) {
+    let computerSelection = computerPlay();
+    let playerSelection = playerInput();
+    console.log(playRound(computerSelection, playerSelection));
+  }
+
+  if (playerScore == computerScore) {
+    console.log("The game is a tie! Wow...");
+  } else if (playerScore > computerScore) {
+    console.log("You win the game! Yay!");
+  } else {
+    console.log("You lose the game! Boo!");
+  }
 }
 
-console.log(playRound(computerSelection, playerSelection));
+game();
